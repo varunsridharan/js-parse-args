e841c3270d29736f7beefe01f5b79cb8d09a11f0
Varun Sridharan <varunsridharan23@gmail.com>
2018-10-31 08:01:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+class JS_Parse_Args {
+	constructor( $args = {}, $defaults = {}, $is_nested = false ) {
+		this.args     = $args;
+		this.defaults = $defaults;
+		this.nested   = $is_nested;
+		this.parse();
+		return this.args;
+	}
+
+	parse() {
+		for( let $_key in this.defaults ) {
+			if( true === this.nested && typeof  this.defaults[ $_key ] === 'object' ) {
+				this.args[ $_key ] = new JS_Parse_Args( this.args[ $_key ], this.defaults[ $_key ], this.nested );
+			} else {
+				if( typeof this.args[ $_key ] === 'undefined' ) {
+					this.args[ $_key ] = this.defaults[ $_key ];
+				}
+			}
+		}
+	}
+}
+
+export default ( $args = {}, $defaults = {}, $is_deep = false ) => new JS_Parse_Args( $args, $defaults, $is_deep );
