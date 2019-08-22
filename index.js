ef80d29f65d4d5d3529eb53abf7980fead82365a
Varun Sridharan <varunsridharan23@gmail.com>
2019-08-22 07:14:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 class JS_Parse_Args {
 	constructor( $args = {}, $defaults = {}, $is_nested = false ) {
-		this.args     = JSON.parse( JSON.stringify( $args ) );
-		this.defaults = JSON.parse( JSON.stringify( $defaults ) );
+		this.args     = JS_Parse_Args.clone( $args );
+		this.defaults = $defaults;
 		this.nested   = $is_nested;
 		this.parse();
 		return this.args;
 	}
 
+	static clone( src ) {
+		if( typeof src === 'object' ) {
+			let target = {};
+			for( let prop in src ) {
+				if( src.hasOwnProperty( prop ) ) {
+					target[ prop ] = src[ prop ];
+				}
+			}
+
+			return target;
+		}
+		return src;
+	}
+
 	parse() {
 		for( let $_key in this.defaults ) {
-			if( true === this.nested && typeof this.defaults[ $_key ] === 'object' ) {
-				this.args[ $_key ] = new JS_Parse_Args( this.args[ $_key ], this.defaults[ $_key ], this.nested );
-			} else {
-				if( typeof this.args[ $_key ] === 'undefined' ) {
-					this.args[ $_key ] = this.defaults[ $_key ];
+			if( this.defaults.hasOwnProperty( $_key ) ) {
+				if( true === this.nested && typeof this.defaults[ $_key ] === 'object' ) {
+					this.args[ $_key ] = new JS_Parse_Args( this.args[ $_key ], this.defaults[ $_key ], this.nested );
+				} else {
+					if( typeof this.args[ $_key ] === 'undefined' ) {
+						this.args[ $_key ] = JS_Parse_Args.clone( this.defaults[ $_key ] );
+					}
 				}
 			}
 		}
